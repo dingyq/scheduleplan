9448121737f0ad10dea3ae299d52700145f88600
yongqiang <yongqiang@futunn.com>
2017-08-23 21:17:16 +0800

    [add] 支持mysql

--- dao/missionNoticeMapping.js
+++ dao/missionNoticeMapping.js
@@ -1,6 +1,6 @@
 var missionNotice = {
 	insert:'INSERT INTO missionnotice(missionId, state, deadlineTime, createUserName, completeTime, type, title, createTime, dutyId, dutyName, createUserId, needClock, content) VALUES(0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-	update:'update missionnotice set state=?, deadlineTime=?, completeTime=?, type=?, title=?, dutyId=?, dutyName=?, needClock=?, content=? where missionId=?',
+	update:'update missionnotice set state=?, deadlineTime=?, completeTime=?, type=?, title=?, dutyId=?, dutyName=?, needClock=?, content=?',
 	delete: 'delete from missionnotice where missionId=?',
 	queryById: 'select * from missionnotice where missionId=?',
 	queryAll: 'select * from missionnotice where createUserId=?'
