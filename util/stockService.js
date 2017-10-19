3f0bf49d121aa8112333c5ffd3364ab47caa742f
panger <pangerding@qq.com>
2017-10-20 00:39:29 +0800

    superagent 转发请求

--- util/stockService.js
+++ util/stockService.js
@@ -1,15 +1,26 @@
-const superagent = require('superagent');
-const timeout = require('connect-timeout');
+// const superagent = require('superagent');
+// const timeout = require('connect-timeout');
 const fs = require('fs');
 const path = require('path');
 
+var Promise = this.Promise || require('promise');
+var agent = require('superagent-promise')(require('superagent'), Promise);
+
 const SEARCH_HOST = 'https://wechatapp.futu5.com/stock/search?';
-const HOST = 'https://wechatapp.futu5.com/stock/kline?';
+const KLINE_DATA_HOST = 'https://wechatapp.futu5.com/stock/kline?';
 
 var stockService = {};
 
+stockService.searchStock = function(req, res, next) {
+  agent('GET', SEARCH_HOST+'keyword=00700').end()
+  .then(function onResult(tRes) { 
+    res.send(tRes.body.data.list[0].stock_id);
+  }, function onError(err) {
+    //err.response has the response from the server 
+    res.send(err);
+  });
 
-var http = require('http');
+}
 
 stockService.fetchKlineChart = function(req, res, next) {
 	// console.log(__dirname);
@@ -18,33 +29,42 @@ stockService.fetchKlineChart = function(req, res, next) {
 	// console.log(path.resolve('./'));
 	console.log(req.originalUrl);
 	
-	var sreq = superagent.get('http://api.douban.com/v2/movie/search?q=' + '宁浩');
-  	// sreq.pipe(res);
-  	// sreq.data();
-  	// sreq.on('end', function (error, res) {
-   //  	console.log('end');
-   //  	console.log(res);
-  	// });
+	// var sreq = superagent.get('http://api.douban.com/v2/movie/search?q=' + '宁浩');
+ //  	// sreq.pipe(res);
+ //  	// sreq.data();
+ //  	// sreq.on('end', function (error, res) {
+ //   //  	console.log('end');
+ //   //  	console.log(res);
+ //  	// });
+ //  	sreq.end(function(err, tRes){
+ //  		// console.log(tRes.body);
+ //  		res.send(tRes.body)
+ //  	});
 
-  	sreq.end(function(err, tRes){
-  		// console.log(tRes.body);
-  		res.send(tRes.body)
-  	});
 
-	// http.get(SEARCH_HOST+'keyword=00700', function(tRes) {
-	// 	res.send(tRes)
-	// });
+    // superagent.get(SEARCH_HOST+'keyword=00700') //give the url
+    // .set('Cookie', 'hello') //setting cookie
+    // .set('user-agent', 'Android') //setting UserAgent
+    // .end(function(error,tRes){
+    //   res.send(tRes.body)
+    //   /* handle the Response(res) or Error (err) */
+    // }.bind(this));
 
-	// var sreq = http.request({
- //        host:     SEARCH_HOST, // 目标主机
- //        path:     '/stock/search？', // 目标路径
- //        method:   req.method // 请求方式
- //    }, function(sres){
- //        sres.pipe(res);
- //        sres.on('end', function(){
- //            console.log('done');
- //        });
- //    });
+
+    agent('GET', SEARCH_HOST+'keyword=00700').end()
+    .then(function onResult(tRes) { 
+        // res.send(tRes.body.data.list[0].stock_id);
+        return tRes.body.data.list[0].stock_id;
+    }, function onError(err) {
+        //err.response has the response from the server 
+        res.send(err);
+    })
+    .then(function(tRes) {
+        res.send(tRes);
+    });
+  
+
+	
 
 
   	// 使用了superagent来发起请求
@@ -57,22 +77,8 @@ stockService.fetchKlineChart = function(req, res, next) {
     // sreq.query({security_id: 210617, market_type:2, data_type:0, kline_type:2, data_set_type:0, data_range_type:3, begin_time:0, end_time:-1, item_count:30});
     // sreq.query('security_id=210617&market_type=2&data_type=0&kline_type=2&data_set_type=0&data_range_type=3&begin_time=0&end_time=-1&item_count=252');
 
-    // sreq.accept('application/json');
-    // sreq.on('end', function() {
-    //     console.log('done');
-    // });
-    // sreq.end(function(res) {
-    // 	console.log('end'+res);
-    // });
 
-   //  superagent.get('https://wechatapp.futu5.com/stock/kline')
-  	// .withCredentials()
-  	// .end(function(res){
-   //  	// assert(200 == res.status);
-   //  	// assert('tobi' == res.text);
-   //  	next();
-   //  	console.log('end'+res);
-  	// })
+   
 
   	// console.log(req.originalUrl)
   	// var superagent = require('superagent');
