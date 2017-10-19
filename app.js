7a59a894efe535b0735e8f82ce0ac15a9c219d9c
yongqiang <yongqiang@futunn.com>
2017-10-19 11:25:49 +0800

    [add] 添加查看股票行情路由

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var stockQuote = require('./routes/stockQuote');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var missionNotice = require('./routes/missionNotice');
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/stock', stockQuote);
 app.use('/users', users);
 app.use('/mission', missionNotice);
 
